adcbf22bda2a75f2405917ad38c223530d1b3d43
Alan Signetti <alansignetti@gmail.com>
2021-05-24 19:52:09 -0300

    Se agregó frontend

---
+++ src/main/java/com/unla/Grupo09OO22021ABM/helpers/ViewRouteHelper.java
@@ -0,0 +1,21 @@
+package com.unla.Grupo09OO22021ABM.helpers;
+
+public class ViewRouteHelper {
+ 
+	/**** Views ****/
+	
+	public final static String INDEX = "/index";
+	
+	
+	//USER
+	public final static String USER_LOGIN = "login";
+	public final static String USER_LOGOUT = "logout";
+	
+	
+	/**** Redirects ****/
+	public final static String HOME = "/home";
+
+	
+
+
+}
