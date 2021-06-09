617f139d54b3fa76eb410259137a4064ef02f0ba
lumerlino <merlinolucia@hotmail.com>
2021-06-08 23:00:33 -0300

    prueba de generarQR

---
+++ src/main/java/com/unla/Grupo09OO22021ABM/util/QRCodeGenerator.java
@@ -0,0 +1,42 @@
+package com.unla.Grupo09OO22021ABM.util;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class QRCodeGenerator {
+	
+	public static void generateQRCodeImage(String text, int width, int height, String filePath)  throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+	
+
+	public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+	    QRCodeWriter qrCodeWriter = new QRCodeWriter();
+	    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+	    ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+	    MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+	    byte[] pngData = pngOutputStream.toByteArray(); 
+	    return pngData;
+	}
+	
+	/*public static BufferedImage generateQRCodeImage(String barcodeText) throws Exception {
+	    QRCodeWriter barcodeWriter = new QRCodeWriter();
+	    BitMatrix bitMatrix = 
+	      barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+
+	    return MatrixToImageWriter.toBufferedImage(bitMatrix);
+	}*/
+
+}
