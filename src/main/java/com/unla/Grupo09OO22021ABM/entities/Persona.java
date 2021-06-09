9272189fb7ecf9f418d02c2aded11ee639364da7
Alan Signetti <alansignetti@gmail.com>
2021-06-08 22:52:01 -0300

    toString de Persona

--- src/main/java/com/unla/Grupo09OO22021ABM/entities/Persona.java
+++ src/main/java/com/unla/Grupo09OO22021ABM/entities/Persona.java
@@ -79,8 +79,7 @@ public class Persona {
 	@Override
 	public String toString() {
 		
-		return "Persona [id_persona=" + id_persona + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni
-				+ "]";
+		return "DNI: " + dni +" ("+ nombre +" "+ apellido +")";
 	}
 
 
