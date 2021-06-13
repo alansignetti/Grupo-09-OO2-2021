0c892f11e300c277d7c175e80ecc69803e723c9b
Fernando <fernandoscroppo@yahoo.com.ar>
2021-06-13 01:49:37 -0300

    Funcionamiento del Logout

--- src/main/java/com/unla/Grupo09OO22021ABM/configuration/SecurityConfiguration.java
+++ src/main/java/com/unla/Grupo09OO22021ABM/configuration/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.unla.Grupo09OO22021ABM.services.implementations.UserDetailsServiceSecurity;
 
@@ -75,9 +76,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				
 				
 			.and()
-				.logout().logoutUrl("/logout").logoutSuccessUrl("/logout")
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+		
+				/*.logout().logoutUrl("/logout").logoutSuccessUrl("/logout").disable().anonymous().
 				.clearAuthentication(true).invalidateHttpSession(true)
-				.deleteCookies("JSESSIONID").permitAll();
+				.deleteCookies("JSESSIONID").permitAll();*/
+				
+				
 		
 	}
 	
